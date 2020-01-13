c69f08fc369e775fe0d70fcdc32e1e8a1b8b681c
Cementator <46320092+Cementator@users.noreply.github.com>
2020-01-13 14:00:06 +0100

    changing local state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,19 +11,37 @@ class Counters extends Component {
     ]
   };
 
-  handleDelete = (counterId) => {
-    console.log("Event Handler Clled", counterId);
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+        c.value = 0;
+        return c;
+    });
+    this.setState({ counters: counters})
   };
 
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-secondary btn-sm"
+          style={this.styles}
+        >Reset</button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
           ></Counter>
         ))}
       </div>
