25dff79e835ba8170b6f1c61e1e9bec9fbbc5e9e
Cementator <46320092+Cementator@users.noreply.github.com>
2020-01-13 13:11:59 +0100

    added multiple counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,69 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-    };
-    styles = {
-        fontSize : 50,
-        fontWeight : 'bold'
-    }
+  state = {
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
+  };
 
-  render() {
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p> There are no tags!</p>;
     return (
-      <React.Fragment>
-        <span style={this.styles}className={ this.getBadgeClasses() }>{this.formatCount()}</span>
-        <button className='btn btn-secondary btn-sm'>Increment</button>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-    getBadgeClasses() {
-        let classes = 'badge m-2 badge-';
-        classes += this.state.count === 0 ? 'warning' : 'primary';
-        return classes;
-    }
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
-  formatCount(){
-      const { count } = this.state;
-      return count === 0 ? 'Zero' : count;
+  render() {
+    return (
+      <div>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+          style={this.styles}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+          style={this.styles}
+        >
+          Delete
+        </button>
+        {/* {this.renderTags()} */}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
