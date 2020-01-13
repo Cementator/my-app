557894b55b155a66a79704f8f02e7fbfa251487a
Cementator <46320092+Cementator@users.noreply.github.com>
2020-01-13 22:59:04 +0100

    added navbar, moved state to app

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,54 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  styles = {
-    fontSize: 30,
-    fontWeight: "bold"
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
-
-  handleIncrement = counter => {
-      const counters = [...this.state.counters];
-      const index = counters.indexOf(counter);
-      counters[index] = { ...counter };
-      counters[index].value++;
-      this.setState({ counters: counters});
-  }
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-        c.value = 0;
-        return c;
-    });
-    this.setState({ counters: counters})
-  };
 
   render() {
+      const { onReset, counters, onDelete, onIncrement} = this.props; //destructuring
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={onReset}
           className="btn btn-secondary btn-sm"
           style={this.styles}
         >Reset</button>
-        {this.state.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           ></Counter>
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,8 +4,10 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
+{/* eslint-disable-next-line  */}
+        <a className="navbar-brand" href="#"> 
+          Navbar {""}
+          <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
         </a>
       </nav>
     );
